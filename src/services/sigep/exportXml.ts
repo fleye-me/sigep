e7bdaaebf9b9aa1c99370794bfcec6da67829798
Diorgeles <diorgelesdias@gmail.com>
2020-06-17 00:25:43 -0300

    function to return XML

---
+++ src/services/sigep/exportXml.ts
@@ -0,0 +1,33 @@
+import { js2xml } from 'xml-js';
+import { IPLP } from '../preListaDePostagem/plp';
+import { parseXml } from 'libxmljs';
+import { xml2string } from '../../utils/xmlValidator';
+
+export default async function exportXml(xml: IPLP) {
+  const objeto_postal_length = xml.correioslog.objeto_postal.length;
+
+  if (objeto_postal_length > 1000) {
+    return 'Só é permitido enviar 1000 encomendas por PLP';
+  } else if (objeto_postal_length === 0) {
+    return 'Adicione pelo menos 1 encomenda';
+  }
+
+  const JS2XML =
+    '<?xml version="1.0" encoding="ISO-8859-1" ?>' +
+    js2xml(xml, {
+      compact: true,
+      ignoreComment: true,
+      spaces: 0,
+    });
+
+  const XMLValidator = parseXml(xml2string);
+  const XMLDocValidator = parseXml(JS2XML);
+
+  return new Promise((resolve, reject: any) => {
+    if (XMLDocValidator.validate(XMLValidator)) {
+      resolve(JS2XML);
+    } else {
+      reject(XMLDocValidator.validationErrors);
+    }
+  });
+}
