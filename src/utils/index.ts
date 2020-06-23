022ec12124e0c562d009567010c9918c12739b7b
Diorgeles <diorgelesdias@gmail.com>
2020-06-23 18:40:18 -0300

    fix range etiquetas

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -15,3 +15,17 @@ export function etiquetasComDigito(etiquetas: Array<string>) {
 export function etiquetasSemEspaco(etiquetas: Array<string>) {
   return etiquetas.map((etiqueta) => etiqueta.replace(' ', ''));
 }
+
+export function etiquetasRangeComDigito(etiquetas: Array<string>) {
+  const tags = etiquetas;
+  const inicial = parseInt(tags[0].substring(2, 10));
+  const final = parseInt(tags[1].substring(2, 10));
+  const prefix = tags[0].substring(0, 2);
+  const sufix = tags[0].substring(10).trim();
+
+  const returnTags = [];
+  for (let i = inicial; i <= final; i++) {
+    returnTags.push(digitoVerificador(`${prefix}${i} ${sufix}`));
+  }
+  return returnTags;
+}
