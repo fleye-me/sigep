4f6ceb9728a13440753601c12178b8b9ab2e3831
Diorgeles <diorgelesdias@gmail.com>
2020-06-24 16:36:24 -0300

    add function to return range

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -16,16 +16,37 @@ export function etiquetasSemEspaco(etiquetas: Array<string>) {
   return etiquetas.map((etiqueta) => etiqueta.replace(' ', ''));
 }
 
+/**@desc Função que retorna todas as etiquetas de acordo com o range passado.
+ * Na montagem do PLP no campo <numero_etiqueta> é necessário o envio das
+ * etiquetas com o Digito
+ */
 export function etiquetasRangeComDigito(etiquetas: Array<string>) {
-  const tags = etiquetas;
-  const inicial = parseInt(tags[0].substring(2, 10));
-  const final = parseInt(tags[1].substring(2, 10));
-  const prefix = tags[0].substring(0, 2);
-  const sufix = tags[0].substring(10).trim();
-
-  const returnTags = [];
-  for (let i = inicial; i <= final; i++) {
-    returnTags.push(digitoVerificador(`${prefix}${i} ${sufix}`));
-  }
-  return returnTags;
+  return etiquetasRange(etiquetas, true);
 }
+
+/**@desc Função que retorna todas as etiquetas de acordo com o range passado.
+ * Na montagem do PLP no campo <listaEtiquetas> é necessário o envio das
+ * etiquetas sem DIGITO e sem ESPAÇO
+ */
+export function etiquetasRangeSemEspaco(etiquetas: Array<string>) {
+  return etiquetasRange(etiquetas, false);
+}
+
+const etiquetasRange = (etiquetas: Array<string>, isDigit: boolean) => {
+  const inicio = parseInt(etiquetas[0].substring(2, 10));
+  const fim = parseInt(etiquetas[1].substring(2, 10));
+  const prefix = etiquetas[0].substring(0, 2);
+  const sufix = etiquetas[0].substring(10).trim();
+
+  const etiquetasList = [];
+  if (isDigit) {
+    for (let etiqueta = inicio; etiqueta <= fim; etiqueta++) {
+      etiquetasList.push(digitoVerificador(`${prefix}${etiqueta} ${sufix}`));
+    }
+  } else {
+    for (let etiqueta = inicio; etiqueta <= fim; etiqueta++) {
+      etiquetasList.push(`${prefix}${etiqueta}${sufix}`);
+    }
+  }
+  return etiquetasList;
+};
